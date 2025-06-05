45ed5629866e3dd36f2ba9cb0e7e95ae342102bc
Kanishka <akcadikari@std.appsc.sab.ac.lk>
2025-06-05 22:24:19 +0530

    day 19 🙌

--- src/pages/client/cart.jsx
+++ src/pages/client/cart.jsx
@@ -7,8 +7,8 @@ export default function CartPage(){
     const [cart,setCart] = useState(getCart())
 
     return(
-        <div className="w-full h-full flex flex-col items-center pt-4 relative ">
-            <div className="w-[400px] h-[80px] shadow-2xl absolute top-1 right-1 flex flex-col justify-center items-center">
+        <div className="w-full max-w-full  h-full flex flex-col items-center pt-4 relative ">
+            <div className="z-50 hidden  w-[400px] h-[80px] shadow-2xl absolute bottom-1 md:top-1 right-1 md:flex flex-col justify-center items-center">
                 <p className="text-2xl text-secondary font-bold">Total: 
                     <span className="text-accent font-bold mx-2">
                         {getTotal().toFixed(2)}
@@ -16,7 +16,8 @@ export default function CartPage(){
                 </p>
                 <Link to="/checkout" state={
                     {
-                        cart: cart           //click the checkout button after send the cart data
+                        cart: cart        //click the checkout button after send the cart data
+
                     }
                 } className="text-white bg-accent px-4 py-2 rounded-lg font-bold hover:bg-secondary transition-all duration-300">
                     Checkout
@@ -26,9 +27,9 @@ export default function CartPage(){
                 cart.map(
                     (item)=>{
                         return(
-                            <div key={item.productId} className="w-[600px] my-4 h-[100px] rounded-tl-3xl rounded-bl-3xl bg-primary shadow-2xl flex flex-row relative justify-center items-center">
+                            <div key={item.productId} className="w-[70%] md:w-[600px] my-4 md:h-[100px] rounded-tl-3xl rounded-bl-3xl bg-primary shadow-2xl flex flex-col md:flex-row relative justify-center items-center p-2 md:pt-0">
                                 <img src={item.image} className="w-[100px] h-[100px] object-cover rounded-3xl"/>
-                                <div className="w-[250px] h-full flex flex-col justify-center items-start pl-4">
+                                <div className="w-[250px] h-full flex flex-col justify-center  items-center md:items-start pl-4">
                                     <h1 className="text-xl text-secondary font-semibold">{item.name}</h1>
                                     <h1 className="text-md text-gray-600 font-semibold">{item.productId}</h1>
                                     {
@@ -48,12 +49,12 @@ export default function CartPage(){
                                     }}><BiMinus/></button>
                                     <h1 className="text-xl text-secondary font-semibold h-full flex items-center">{item.qty}</h1>
                                     <button className="text-white font-bold rounded-xl hover:bg-secondary p-2 text-xl cursor-pointer  aspect-square bg-accent" onClick={()=>{
-                                        addToCart(item , 1)// item add to cart
-                                        setCart(getCart())//get new item and refesh page 
+                                        addToCart(item , 1)    // item add to cart
+                                        setCart(getCart())    //get new item and refesh page
                                     }}><BiPlus/></button>                                
                                 </div>
                                 {/* total */}
-                                <div className="w-[200px] h-full flex flex-col justify-center items-end pr-4">
+                                <div className="w-[200px] h-full flex flex-col justify-center items-center md:items-end pr-4">
                                     <h1 className="text-2xl text-secondary font-semibold">Rs. {(item.price*item.qty).toFixed(2)}</h1>
                                 </div>
                                 <button className="absolute text-red-600 cursor-pointer hover:bg-red-600 hover:text-white rounded-full p-2 right-[-35px] " onClick={
@@ -69,6 +70,20 @@ export default function CartPage(){
                     }
                 )
             }
+            <div className="z-50 md:hidden  flex w-full h-[100px] shadow-2xl    flex-col justify-center items-center">
+                <p className="text-2xl text-secondary font-bold">Total: 
+                    <span className="text-accent font-bold mx-2">
+                        {getTotal().toFixed(2)}
+                    </span>
+                </p>
+                <Link to="/checkout" state={
+                    {
+                        cart: cart
+                    }
+                } className="text-white bg-accent px-4 py-2 rounded-lg font-bold hover:bg-secondary transition-all duration-300">
+                    Checkout
+                </Link>
+            </div>
         </div>
     )
 }
--- src/pages/client/productOverView.jsx
+++ src/pages/client/productOverView.jsx
@@ -30,13 +30,8 @@ export default function ProductOverviewPage() {
 	return (
 		<>
 			{status == "success" && (
-				<div className="w-full h-full flex">
-					<div className="w-[50%]  h-full flex justify-center items-center">
-						<ImageSlider images={product.images} />
-					</div>
-					<div className="w-[50%] flex justify-center items-center h-full">
-						<div className="w-[500px] h-[600px] flex flex-col  items-center">
-							<h1 className="w-full text-center text-4xl text-secondary font-semibold">
+				<div className="w-full h-full flex flex-col md:flex-row md:max-h-full md:overflow-y-scroll pt-4">
+					<h1 className="w-full md:hidden block my-8 text-center text-4xl text-secondary font-semibold">
 								{product.name}
 								{product.altNames.map((altName, index) => {
 									return (
@@ -47,7 +42,23 @@ export default function ProductOverviewPage() {
 									);
 								})}
 							</h1>
-							{/* product Id */}
+					<div className="w-full md:w-[50%]  md:h-full flex justify-center ">
+						<ImageSlider images={product.images} />
+					</div>
+					<div className="w-full md:w-[50%] flex justify-center  md:h-full ">
+						<div className="w-full md:w-[500px] md:h-[600px] flex flex-col  items-center ">
+							<h1 className="w-full hidden md:block text-center text-4xl text-secondary font-semibold">
+								{product.name}
+								{product.altNames.map((altName, index) => {
+									return (
+										<span key={index} className="text-4xl text-gray-600 ">
+											{" "}
+											{" | " + altName}
+										</span>
+									);
+								})}
+							</h1>
+							
 							<h1 className="w-full text-center my-2 text-md text-gray-600 font-semibold">
 								{product.productId}
 							</h1>
@@ -68,7 +79,7 @@ export default function ProductOverviewPage() {
 									{product.price.toFixed(2)}
 								</span>
 							)}
-							<div className="w-full flex justify-center items-center mt-4">
+							<div className="w-full flex flex-col md:flex-row gap-2 justify-center items-center mt-4">
 								<button
 									className="w-[200px] h-[50px] mx-4 cursor-pointer bg-accent text-white rounded-2xl hover:bg-accent/80 transition-all duration-300"
 									onClick={() => {

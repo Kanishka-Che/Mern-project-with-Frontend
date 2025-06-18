0f15042472ce34639693c0669aecd7345f88554a
Kanishka <akcadikari@std.appsc.sab.ac.lk>
2025-06-18 18:40:50 +0530

    oauth login modify

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,39 @@
-import { BrowserRouter, Routes,Route } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Header from './components/header'
-import ProductCard from './components/productCard';
-import HomePage from './pages/home';
-import LoginPage from './pages/login';
-import AdminPage from './pages/adminPage';
-import TestPage from './pages/testPage';
-import { Toaster } from 'react-hot-toast';
-import RegisterPage from './pages/register';
+import ProductCard from './components/productCard'
+import HomePage from './pages/home'
+import LoginPage from './pages/login'
+import AdminPage from './pages/adminPage'
+import TestPage from './pages/testPage'
+import { Toaster } from 'react-hot-toast'
+import RegisterPage from './pages/register'
 import { GoogleOAuthProvider } from '@react-oauth/google';
-
+import ForgetPasswordPage from './pages/forgetPassword'
 
 function App() {
-  
+ 
 
   return (
-   <GoogleOAuthProvider clientId="449185289518-i4air9db7fesl39fv7ju4ifs4i6ojequ.apps.googleusercontent.com">...
+    <GoogleOAuthProvider clientId="37337765557-5qck0erqtvg70n7fuldt6t82ovtvgrfo.apps.googleusercontent.com">
     <BrowserRouter>
-        <div>
-          <Toaster position='top-right'/> 
-          <Routes path="/*">
-             <Route path="/login" element={<LoginPage/>}/>
-             <Route path="/signup" element={<RegisterPage/>}/>
-             <Route path="/testing"  element={<TestPage/>}/>
-             <Route path="/admin/*" element={<AdminPage/>}/>
-             <Route path="/*" element={<HomePage/>}/>
-
-            
-          </Routes>
-        </div>
+      <div >
+        <Toaster position='top-right'/>
+        {/* <Header/> */}
+        <Routes path="/*">
+          <Route path='/login' element={<LoginPage/>}/>
+          <Route path="/forget" element={<ForgetPasswordPage/>}/>
+          <Route path="/signup" element={<RegisterPage/>}/>
+          <Route path="/testing" element={<TestPage/>}/>
+          <Route path='/admin/*' element={<AdminPage/>}/>
+          <Route path='/*' element={<HomePage/>}/>
+        </Routes>
+      </div>
     </BrowserRouter>
-    </GoogleOAuthProvider>
-    
+   </GoogleOAuthProvider>
   )
 }
 
-export default App;
-
-
+export default App
 //https://acqkjhucvmtoppzmtnns.supabase.co
 //eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFjcWtqaHVjdm10b3Bwem10bm5zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY5Mzg1OTksImV4cCI6MjA2MjUxNDU5OX0.cI1PurCfwyjef0X4wDsvWVyCkCuMTQtz73oAEMo7EoQ
